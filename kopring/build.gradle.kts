b84ba4a6b2e4dc44b283bb705659b641c0c42deb
doljae <seok9211@naver.com>
2022-03-04 00:03:45 +0900

    update build script for submodules

--- kopring/build.gradle.kts
+++ kopring/build.gradle.kts
@@ -2,13 +2,6 @@ plugins {
     kotlin("jvm") version "1.6.10"
 }
 
-group = "com.example"
-version = "0.0.1-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
-
 dependencies {
     implementation(kotlin("stdlib"))
 }
