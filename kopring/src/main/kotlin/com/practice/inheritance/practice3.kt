fd957dcfca4d4781cd7316b3a64e7825f56bb7cf
doljae <seok9211@naver.com>
2022-03-12 23:17:51 +0900

    Calling the superclass implementation with kotlin

---
+++ kopring/src/main/kotlin/com/practice/inheritance/practice3.kt
@@ -0,0 +1,30 @@
+package com.practice.inheritance
+
+open class Rectangle1 {
+    open fun draw() {
+        println("Drawing a rectangle")
+    }
+
+    val borderColor: String get() = "black"
+}
+
+class FilledRectangle1 : Rectangle1() {
+    override fun draw() {
+        super.draw()
+        println("Filling the rectangle")
+    }
+
+    val fillColor: String get() = super.borderColor
+
+    inner class Filler {
+        fun fill() {
+            println("Filling")
+        }
+
+        fun drawAndFill() {
+            super@FilledRectangle1.draw()
+            fill()
+            println("Drawn a filled rectangle with color ${super@FilledRectangle1.borderColor}")
+        }
+    }
+}
