9f30b794b02686d4be04c233548cbb44478f6cc9
doljae <seok9211@naver.com>
2022-03-15 23:50:21 +0900

    declaring properties using kotlin

---
+++ kopring/src/test/kotlin/com/example/practice/properties/practice1.kt
@@ -0,0 +1,20 @@
+package com.example.practice.properties
+
+class Address {
+    var name: String = "holmes, Sherlock"
+    var street: String = "Baker"
+    var city: String = "London"
+    var state: String? = null
+    var zip: String = "123456"
+}
+
+fun copyAddress(address: Address): Address {
+    val result = Address()
+    result.name = address.name
+    result.street = address.street
+    result.city = address.city
+    result.state = address.state
+    result.zip = address.zip
+
+    return result
+}
