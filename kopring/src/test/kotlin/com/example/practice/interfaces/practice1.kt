f7f189563275593fbd9e069d7cc7aff9e2cf2c14
doljae <seok9211@naver.com>
2022-03-17 18:29:59 +0900

    properties in interfaces

--- kopring/src/test/kotlin/com/example/practice/interfaces/practice1.kt
+++ kopring/src/test/kotlin/com/example/practice/interfaces/practice1.kt
@@ -1,11 +1,19 @@
 package com.example.practice.interfaces
 
 interface MyInterface {
+    val prop: Int
+    val propertyWithImplementation: String
+        get() = "foo"
+
     fun bar()
-    fun foo() {}
+    fun foo() {
+        print(prop)
+    }
 }
 
-class Chile:MyInterface{
+class Child : MyInterface {
+    override val prop: Int = 29
+
     override fun bar() {
         TODO("Not yet implemented")
     }
