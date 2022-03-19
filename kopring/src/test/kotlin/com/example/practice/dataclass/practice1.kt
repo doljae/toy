d3a0f59ff88f2f6a205504325de4ad848212aad2
doljae <seok9211@naver.com>
2022-03-19 23:10:14 +0900

    easy object copy using Object.copy() in kotlin

--- kopring/src/test/kotlin/com/example/practice/dataclass/practice1.kt
+++ kopring/src/test/kotlin/com/example/practice/dataclass/practice1.kt
@@ -8,11 +8,19 @@ data class Person(val name: String) {
     var age: Int = 0
 }
 
+
 fun main() {
     val person1 = Person("John")
     val person2 = Person("John")
     person1.age = 10
     person2.age = 10
+
+    val oldPerson1 = person1.copy(name = "oldJohn")
+
+    val jane = User2("Jane", 35)
+    val (name, age) = jane
+
+    println("$name, $age years of age")
 
     println(person1 == person2)
 }
