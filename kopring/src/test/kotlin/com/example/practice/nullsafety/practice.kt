8476965858ef4506896ddf808391e84f9fe5d9bf
doljae <seok9211@naver.com>
2022-03-24 19:26:58 +0900

    elvis operator in kotlin

--- kopring/src/test/kotlin/com/example/practice/nullsafety/practice.kt
+++ kopring/src/test/kotlin/com/example/practice/nullsafety/practice.kt
@@ -1,5 +1,7 @@
 package com.example.practice.nullsafety
 
+import org.w3c.dom.Node
+
 var a: String = "abc"
 
 var b: String? = "abc"
@@ -17,5 +19,16 @@ fun main() {
     } else {
         println("Empty string")
     }
+
+    // elvis
+
+    val f: Int = if (b != null) b.length else -1
+    val ff = b?.length ?: -1
+
 }
 
+fun foo(node: Node): String? {
+    val parent = node.nodeName ?: return null
+    val name = node.nodeName ?: throw IllegalArgumentException()
+    return "test"
+}
