a1e093a65a96c6359a14d8d34ca8ab277cf5f5cf
doljae <seok9211@naver.com>
2022-03-29 21:49:10 +0900

    add dependency for spring boot in kotlin project

--- kopring/build.gradle.kts
+++ kopring/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 tasks.getByName<Jar>("jar") {
     enabled = true
 }
@@ -6,11 +8,24 @@ tasks.getByName<Jar>("bootJar") {
     enabled = false
 }
 
+//group = "com.example"
+//version = "0.0.1-SNAPSHOT"
+//java.sourceCompatibility = JavaVersion.VERSION_17
+
 plugins {
+    id("org.springframework.boot") version "2.6.5"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.6.10"
+    kotlin("plugin.spring") version "1.6.10"
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
     implementation(kotlin("stdlib"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
 
@@ -19,6 +34,17 @@ dependencies {
     testImplementation("io.kotest:kotest-property")
 }
 
-tasks.withType<Test>().configureEach {
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
+}
+
+tasks.withType<Test> {
     useJUnitPlatform()
 }
+
+//tasks.withType<Test>().configureEach {
+//    useJUnitPlatform()
+//}
