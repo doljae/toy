c368d48f1b9073059fa0089e73775c1a8b9511ae
doljae <seok9211@naver.com>
2022-03-31 23:07:33 +0900

    add test api controller with kotlin & spring

---
+++ kopring/src/main/kotlin/com/example/controller/TestController.kt
@@ -0,0 +1,20 @@
+package com.example.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class TestController {
+
+    @GetMapping("/test")
+    fun testApi(): String {
+        return "it works!"
+    }
+
+    @GetMapping("/test2/{id}")
+    fun testApi2(@PathVariable id: String) {
+        println("path variable id:$id")
+    }
+
+}
