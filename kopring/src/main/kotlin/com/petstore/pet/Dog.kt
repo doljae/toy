ecae13b4a892af6c3cd1c4d68cac32d16fdae2b9
doljae <seok9211@naver.com>
2022-04-11 13:20:58 +0900

    add abstract method toString(), cry(), implement methods

--- kopring/src/main/kotlin/com/petstore/pet/Dog.kt
+++ kopring/src/main/kotlin/com/petstore/pet/Dog.kt
@@ -1,5 +1,16 @@
 package com.petstore.pet
 
-class Dog(name: String, age: Int, gender: Enum<Gender>, breed: Breed) : Pet(name, age, gender) {
+class Dog(
+    name: String,
+    age: Int,
+    gender: Enum<Gender>,
+    private val breed: Enum<Breed>
+) : Pet(name, age, gender) {
+    override fun cry() {
+        println("왈왈")
+    }
 
+    override fun toString(): String {
+        return super.toString() + ", breed: $breed"
+    }
 }
--- kopring/src/main/kotlin/com/petstore/pet/Pet.kt
+++ kopring/src/main/kotlin/com/petstore/pet/Pet.kt
@@ -1,6 +1,13 @@
 package com.petstore.pet
 
-abstract class Pet(val name: String,
-                   val age: Int,
-                   val gender: Enum<Gender>) {
+abstract class Pet(
+    private val name: String,
+    private val age: Int,
+    private val gender: Enum<Gender>
+) {
+    abstract fun cry()
+
+    override fun toString(): String {
+        return "${this.name}, name: $name, age: $age, gender: $gender"
+    }
 }
