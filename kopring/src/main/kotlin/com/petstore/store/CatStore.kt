701755c0da72f9a834f2ba14b0180042ba458519
doljae <seok9211@naver.com>
2022-04-12 22:08:25 +0900

    add CatStore

---
+++ kopring/src/main/kotlin/com/petstore/store/CatStore.kt
@@ -0,0 +1,29 @@
+package com.petstore.store
+
+import com.petstore.pet.Cat
+import com.petstore.pet.Pet
+
+class CatStore : Store {
+
+    private var cages = mutableListOf<Cat>()
+
+    override fun addPet(pet: Pet) {
+        cages.add(pet as Cat)
+    }
+
+    override fun getPet(id: Int): Cat {
+        return cages[id]
+    }
+
+    override fun removePet(id: Int) {
+        cages.removeAt(id)
+    }
+
+    override fun updatePet(id: Int, pet: Pet) {
+        cages[id] = pet as Cat
+    }
+
+    override fun toString(): String {
+        return "${this.javaClass.name}, cages: $cages"
+    }
+}
