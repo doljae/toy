32d5794354bd8b12366b9065aec1265ecd4c6898
doljae <seok9211@naver.com>
2022-04-12 22:09:51 +0900

    test Cat & CatStore

--- kopring/src/main/kotlin/com/petstore/PetStoreMain.kt
+++ kopring/src/main/kotlin/com/petstore/PetStoreMain.kt
@@ -1,16 +1,24 @@
 package com.petstore
 
 import com.petstore.pet.Breed
+import com.petstore.pet.Cat
 import com.petstore.pet.Dog
 import com.petstore.pet.Gender
+import com.petstore.store.CatStore
 import com.petstore.store.DogStore
 
 fun main() {
     val dogStore = DogStore()
     val dog = Dog("teddy", 10, Gender.MALE, Breed.HUSKY)
-
     dogStore.addPet(dog)
 
     println(dog)
     println(dogStore)
+
+    val catStore = CatStore()
+    val cat = Cat("lulu", 8, Gender.MALE, Breed.SAMOYED)
+    catStore.addPet(cat)
+
+    println(cat)
+    println(catStore)
 }
