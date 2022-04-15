3023adc02f7e1a22a4eda32b157d88ed03f67448
doljae <seok9211@naver.com>
2022-04-15 12:57:20 +0900

    add IntStream, check default ForkJoinPool-CommonPool size, based on the number of cpu-cores

--- japring/src/main/java/com/example/test/FolkJoinPoolTest.java
+++ japring/src/main/java/com/example/test/FolkJoinPoolTest.java
@@ -7,28 +7,44 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.stream.IntStream;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class FolkJoinPoolTest {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        final ExecutorService executorService = Executors.newFixedThreadPool(5);
-        final List<Future<String>> futures = new ArrayList<>();
+        // use ExecutorService
+//        log.info("use ExecutorService");
+//        final ExecutorService executorService = Executors.newFixedThreadPool(5);
+//        final List<Future<String>> futures = new ArrayList<>();
+//
+//        for (int i = 0; i < 10; i++) {
+//            final int index = i;
+//            futures.add(executorService.submit(() -> {
+//                Thread.sleep(5000);
+//                return Thread.currentThread().getName() + ", index=" + index + ", ended at " + new Date();
+//            }));
+//        }
+//
+//        for (Future<String> future : futures) {
+//            final String result = future.get();
+//            log.info("Thread result: " + result);
+//        }
+//        executorService.shutdown();
 
-        for (int i = 0; i < 10; i++) {
-            final int index = i;
-            futures.add(executorService.submit(() -> {
+        // use stream + parallel()
+        log.info("use stream + parallel()");
+        IntStream.range(0, 20).parallel().forEach(index -> {
+            System.out.println(
+                "Starting " + Thread.currentThread().getName() + ",    index=" + index + ", " + new Date());
+            try {
                 Thread.sleep(5000);
-                return Thread.currentThread().getName() + ", index=" + index + ", ended at " + new Date();
-            }));
-        }
+            } catch (InterruptedException e) {
+                log.error("InterruptedException", e);
+            }
+        });
 
-        for (Future<String> future : futures) {
-            final String result = future.get();
-            log.info("Thread result: " + result);
-        }
-        executorService.shutdown();
     }
 }
 
