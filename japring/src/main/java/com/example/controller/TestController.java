f4dc8d74153980bc3734ab8ccee0855c84f03e33
doljae <seok9211@naver.com>
2022-04-22 22:07:16 +0900

    add test api which exposed under specific profiles

--- japring/src/main/java/com/example/controller/TestController.java
+++ japring/src/main/java/com/example/controller/TestController.java
@@ -3,6 +3,7 @@ package com.example.controller;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import com.example.domain.Color;
 import com.example.dto.ResponseDto;
 
 @RestController
+@Profile("default")
 public class TestController {
 
     @GetMapping("/test")
