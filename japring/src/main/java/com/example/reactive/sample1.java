e0c9ba63e8f414e04390386becfe669f40a2eff8
doljae <seok9211@naver.com>
2022-04-24 13:05:19 +0900

    add RxJava library, add sample code using Flowable

---
+++ japring/src/main/java/com/example/reactive/sample1.java
@@ -0,0 +1,11 @@
+package com.example.reactive;
+
+import io.reactivex.rxjava3.core.Flowable;
+
+public class sample1 {
+
+    public static void main(String[] args) {
+        final Flowable<String> flowable = Flowable.just("Hello", "World");
+        flowable.subscribe(System.out::println);
+    }
+}
