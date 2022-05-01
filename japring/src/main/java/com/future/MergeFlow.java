5a770dc1ff31ed13264a30c958b680780fef9c82
doljae <seok9211@naver.com>
2022-05-01 13:16:48 +0900

    implement Publisher, Subscriber to SimpleCell

--- japring/src/main/java/com/future/MergeFlow.java
+++ japring/src/main/java/com/future/MergeFlow.java
@@ -4,7 +4,12 @@ public class MergeFlow {
 
     public static void main(String[] args) {
 
+        final SimpleCell c3 = new SimpleCell("C3");
         final SimpleCell c2 = new SimpleCell("C2");
         final SimpleCell c1 = new SimpleCell("C1");
+
+        c1.subscribe(c3);
+        c1.onNext(10);
+        c2.onNext(20);
     }
 }
--- japring/src/main/java/com/future/SimpleCell.java
+++ japring/src/main/java/com/future/SimpleCell.java
@@ -1,10 +1,44 @@
 package com.future;
 
-public class SimpleCell {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Flow.Publisher;
+import java.util.concurrent.Flow.Subscriber;
+import java.util.concurrent.Flow.Subscription;
+
+public class SimpleCell implements Publisher<Integer>, Subscriber<Integer> {
     private int value = 0;
     private String name;
+    private List<Subscriber> subscribers = new ArrayList<>();
 
     public SimpleCell(String name) {
         this.name = name;
+    }
+
+    @Override
+    public void subscribe(Subscriber<? super Integer> subscriber) {
+        subscribers.add(subscriber);
+    }
+
+    @Override
+    public void onSubscribe(Subscription subscription) {
+
+    }
+
+    @Override
+    public void onNext(Integer newValue) {
+        value = newValue;
+        System.out.println(name + ":" + value);
+        subscribers.forEach(subscriber -> subscriber.onNext(value));
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+
+    }
+
+    @Override
+    public void onComplete() {
+
     }
 }
