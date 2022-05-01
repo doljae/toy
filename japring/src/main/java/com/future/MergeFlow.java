a7e3ea3c1a951322855405580bccf1987f56bb4d
doljae <seok9211@naver.com>
2022-05-01 13:08:01 +0900

    add SimpleCell

---
+++ japring/src/main/java/com/future/MergeFlow.java
@@ -0,0 +1,10 @@
+package com.future;
+
+public class MergeFlow {
+
+    public static void main(String[] args) {
+
+        final SimpleCell c2 = new SimpleCell("C2");
+        final SimpleCell c1 = new SimpleCell("C1");
+    }
+}
---
+++ japring/src/main/java/com/future/SimpleCell.java
@@ -0,0 +1,10 @@
+package com.future;
+
+public class SimpleCell {
+    private int value = 0;
+    private String name;
+
+    public SimpleCell(String name) {
+        this.name = name;
+    }
+}
