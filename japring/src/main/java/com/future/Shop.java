2d41270d0ebdf36c5e0b81baaa19f7678c82be18
doljae <seok9211@naver.com>
2022-05-02 11:20:02 +0900

    add method for simulaing IO event using Thread.sleep(1000L)

---
+++ japring/src/main/java/com/future/Shop.java
@@ -0,0 +1,23 @@
+package com.future;
+
+import java.util.Random;
+
+public class Shop {
+
+    public double getPrice(String product) {
+        return calculatePrice(product);
+    }
+
+    private double calculatePrice(String product) {
+        delay();
+        return new Random().nextDouble() * product.charAt(0) + product.charAt(1);
+    }
+
+    private void delay() {
+        try {
+            Thread.sleep(1000L);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
