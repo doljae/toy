e6e914ce11ce3f95acf6acddc79d8cfc62af9374
doljae <seok9211@naver.com>
2022-05-03 11:41:25 +0900

    add parallel method with parallelStream to check performance efficiency

--- japring/src/main/java/com/future/BlockingShop.java
+++ japring/src/main/java/com/future/BlockingShop.java
@@ -16,11 +16,23 @@ public class BlockingShop {
                                                         shop.getPrice(product))).toList();
     }
 
+    // parallelStream을 이용한 병렬 처리, 1개의 shop을 조회하는 시간보다 아주 약간 더 걸린다.
+    static List<String> findPricesV2(String product) {
+        return shops.parallelStream().map(shop -> String.format("%s price is %.2f", shop.getShopName(),
+                                                                shop.getPrice(product))).toList();
+    }
+
     public static void main(String[] args) {
-        System.out.println("==== start ====");
+        System.out.println("==== blocking method start ====");
         final long start = System.nanoTime();
         System.out.println(findPrices("iPhone30"));
         final long duration = (System.nanoTime() - start) / 1_000_000;
         System.out.println("Done in " + duration + " msecs");
+
+        System.out.println("==== use parallelStream start ====");
+        final long start2 = System.nanoTime();
+        System.out.println(findPricesV2("iPhone30"));
+        final long duration2 = (System.nanoTime() - start2) / 1_000_000;
+        System.out.println("Done in " + duration2 + " msecs");
     }
 }
