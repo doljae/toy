26f11fe8dea803d9583519c3d98c156d8e040b59
doljae <seok9211@naver.com>
2022-05-13 23:23:14 +0900

    disable globally applying dependency-management plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension
 plugins {
     java
     id("org.springframework.boot") version "2.6.7" apply false
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE" apply false
     kotlin("jvm") version "1.6.21" apply false
     kotlin("plugin.spring") version "1.6.21" apply false
     kotlin("plugin.jpa") version "1.6.21" apply false
