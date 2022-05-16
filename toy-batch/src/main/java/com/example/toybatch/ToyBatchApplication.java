660aa3b1393a5d973ff047e49beca30704143ecf
doljae <seok9211@naver.com>
2022-05-16 10:45:40 +0900

    add batch module

---
+++ toy-batch/src/main/java/com/example/toybatch/ToyBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.toybatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyBatchApplication.class, args);
+    }
+
+}
---
+++ toy-batch/src/test/java/com/example/toybatch/ToyBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toybatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
