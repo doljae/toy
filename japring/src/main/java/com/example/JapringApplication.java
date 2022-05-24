4de99b4e742824e2e5a99561a7f4a8bd4958f12b
doljae <seok9211@naver.com>
2022-05-24 10:55:50 +0900

    add @EnableWebFlux annotation

--- japring/src/main/java/com/example/JapringApplication.java
+++ japring/src/main/java/com/example/JapringApplication.java
@@ -3,7 +3,9 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
+@EnableWebFlux
 @EnableFeignClients
 @SpringBootApplication
 public class JapringApplication {
