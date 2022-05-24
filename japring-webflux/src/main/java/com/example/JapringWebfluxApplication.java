a2e46a874ff1631f6f3ab3a68b24c5245f018901
doljae <seok9211@naver.com>
2022-05-24 11:02:04 +0900

    add japring-webflux submodule

---
+++ japring-webflux/src/main/java/com/example/JapringWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapringWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JapringWebfluxApplication.class, args);
+    }
+
+}
