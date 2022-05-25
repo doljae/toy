11a45adbacf024993a74fa9cd7265cebaccb274c
doljae <seok9211@naver.com>
2022-05-25 20:31:49 +0900

    add netty-all dependency for handling issue from apple silicon & Netty

--- japring-webflux/build.gradle.kts
+++ japring-webflux/build.gradle.kts
@@ -9,7 +9,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     // https://stackoverflow.com/questions/71966221/spring-api-gateway-m1-java-lang-unsatisfiedlinkerror-no-netty-resolver-dns-n
     // https://github.com/netty/netty/issues/11020
-    implementation("io.netty:netty-resolver-dns-native-macos:4.1.77.Final:osx-aarch_64")
+    implementation("io.netty:netty-all")
 
     runtimeOnly("mysql:mysql-connector-java")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
