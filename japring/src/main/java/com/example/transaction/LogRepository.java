e0b0cd84a0a34e3f64ac52bfc5246956f0756ed5
doljae <seok9211@naver.com>
2022-06-27 15:53:23 +0900

    add Log, LogRepository

---
+++ japring/src/main/java/com/example/transaction/LogRepository.java
@@ -0,0 +1,36 @@
+package com.example.transaction;
+
+import java.util.Optional;
+
+import javax.persistence.EntityManager;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Repository
+@RequiredArgsConstructor
+public class LogRepository {
+
+    private final EntityManager entityManager;
+
+    @Transactional
+    public void save(Log logMessage) {
+        log.info("log 저장");
+        entityManager.persist(logMessage);
+
+        if (logMessage.getMessage().contains("로그예외")) {
+            log.info("log 저장시 예외 발생");
+            throw new RuntimeException("예외 발생");
+        }
+    }
+
+    public Optional<Log> find(String message) {
+        return entityManager.createQuery("select l from Log l where l.message=:message", Log.class)
+                            .setParameter("message", message)
+                            .getResultList().stream().findAny();
+    }
+}
