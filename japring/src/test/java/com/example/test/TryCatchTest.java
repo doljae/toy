1997a65d590414733b0b5a9c79ced4ba9a2c47eb
doljae <seok9211@naver.com>
2022-07-03 19:38:21 +0900

    if a exception is thrown in try scope and catch scope handle Exception.class, the thrown exception will be catched

---
+++ japring/src/test/java/com/example/test/TryCatchTest.java
@@ -0,0 +1,32 @@
+package com.example.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class TryCatchTest {
+
+    @Test
+    void test() {
+        // fail
+        Assertions.assertThrows(AccountTokenExpiredException.class, this::testMethod);
+    }
+
+    private void testMethod() {
+        try {
+            System.out.println("do something");
+            if (true) {
+                throw new AccountTokenExpiredException();
+            }
+        } catch (Exception e) {
+            throw new AccountNotVerifiedException();
+        }
+    }
+
+    static class AccountTokenExpiredException extends RuntimeException {}
+
+    ;
+
+    static class AccountNotVerifiedException extends RuntimeException {}
+
+    ;
+}
