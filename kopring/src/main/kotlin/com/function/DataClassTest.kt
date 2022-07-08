1ad07a553afb07686a722029549f0a4ccb298463
doljae <seok9211@naver.com>
2022-07-08 14:21:03 +0900

    data class

---
+++ kopring/src/main/kotlin/com/function/DataClassTest.kt
@@ -0,0 +1,16 @@
+package com.function
+
+// getter, setter, toString, equals, hashcode를 신경안쓰고 올바르게 구현되어있음
+data class People(var name: String, var age: Int, var gender: String) {}
+
+fun main() {
+    val people = People("doljae", 30, "male")
+    val copiedPeople = people.copy(name = "eajlod")
+
+    println(people)
+    println(copiedPeople)
+
+    // destructuring
+    val (name, age, gender) = people
+    println("$name $age $gender")
+}
