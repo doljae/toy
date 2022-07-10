15976573b38ee741994030e7173f7b69e708d80c
doljae <seok9211@naver.com>
2022-07-10 15:29:55 +0900

    star projection

--- kopring/src/main/kotlin/com/function/CovarianceTest.kt
+++ kopring/src/main/kotlin/com/function/CovarianceTest.kt
@@ -21,6 +21,32 @@ fun copyFromTo(from: Array<out Fruit>, to: Array<in Fruit>) {
     }
 }
 
+// T에 대한 제약 조건을 한개면 : 를 사용해 명시할 수 있다
+fun <T : AutoCloseable> useAndClose(input: T) {
+    // compile error, T가 close()를 가지고 있는지 명시해줘야함
+    // input.close
+
+    input.close()
+}
+
+// 2개 이상일 때는 where를 사용한다
+fun <T> useAndCloseV2(input: T) where T : AutoCloseable, T : Appendable {
+    input.append("there")
+    input.close()
+}
+
+// java에 ? 와 비슷한 *
+// 타입을 정확히는 모르지만 타입 안정성을 유지하고 읽기만 허용할 때 사용함
+fun printValues(values: Array<*>) {
+    // 읽기만 가능
+    for (value in values) {
+        println(value)
+    }
+
+    // 쓰기는 불가능
+    // values[0] = values[1]
+}
+
 
 
 fun main() {
