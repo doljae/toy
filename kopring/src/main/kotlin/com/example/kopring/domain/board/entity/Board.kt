86b5859b9a06ab6f3fece16f6e45940a84c8ea24
doljae <seok9211@naver.com>
2022-07-17 00:51:43 +0900

    modify directory structure, domain type

--- kopring/src/main/kotlin/com/example/kopring/domain/board/entity/Board.kt
+++ kopring/src/main/kotlin/com/example/kopring/domain/board/entity/Board.kt
@@ -1,4 +1,4 @@
-package com.example.kopring.board.entity
+package com.example.kopring.domain.board.entity
 
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
