4ed581c0fc2c077b276d25d570cce98bcdb19453
doljae <seok9211@naver.com>
2022-07-23 22:06:54 +0900

    add function to service layer for retrieving entity and return dao

--- kopring/src/main/kotlin/com/example/kopring/domain/board/application/BoardService.kt
+++ kopring/src/main/kotlin/com/example/kopring/domain/board/application/BoardService.kt
@@ -1,13 +1,18 @@
 package com.example.kopring.domain.board.application
 
 import com.example.kopring.domain.board.dao.BoardRepository
+import com.example.kopring.domain.board.dao.BoardRepositoryDecorator
 import com.example.kopring.domain.board.dto.BoardRequest
 import com.example.kopring.domain.board.entity.Board
+import com.example.kopring.domain.board.entity.BoardDao
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
-class BoardService(val boardRepository: BoardRepository) {
+class BoardService(
+    val boardRepository: BoardRepository,
+    val boardRepositoryDecorator: BoardRepositoryDecorator
+) {
 
     fun getBoard(id: Long): Board? = boardRepository.findByIdOrNull(id)
     fun getAllBoards(): List<Board> = boardRepository.findAll()
@@ -16,4 +21,6 @@ class BoardService(val boardRepository: BoardRepository) {
         val board = Board(title, author, content)
         boardRepository.save(board)
     }
+
+    fun getBoardDao(id: Long): BoardDao = boardRepositoryDecorator.findById(id)
 }
--- kopring/src/main/kotlin/com/example/kopring/domain/board/dao/BoardRepositoryDecorator.kt
+++ kopring/src/main/kotlin/com/example/kopring/domain/board/dao/BoardRepositoryDecorator.kt
@@ -1,11 +1,13 @@
 package com.example.kopring.domain.board.dao
 
 import com.example.kopring.domain.board.entity.Board
+import com.example.kopring.domain.board.entity.BoardDao
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Repository
 
 @Repository
 class BoardRepositoryDecorator(val boardRepository: BoardRepository) {
 
-    fun findById(id: Long): Board = boardRepository.findByIdOrNull(id) ?: throw RuntimeException()
+    fun findById(id: Long): BoardDao =
+        Board.toDao(boardRepository.findByIdOrNull(id) ?: throw RuntimeException())
 }
