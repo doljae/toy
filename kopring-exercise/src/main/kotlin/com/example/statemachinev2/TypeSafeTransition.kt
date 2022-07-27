c267a8c178d93e1a03321eed0a53adf7d0f07226
doljae <seok9211@naver.com>
2022-07-27 14:57:44 +0900

    state machine implementation with states with typesafe transition

---
+++ kopring-exercise/src/main/kotlin/com/example/statemachinev2/TypeSafeTransition.kt
@@ -0,0 +1,28 @@
+package com.example.statemachinev2
+
+import ru.nsk.kstatemachine.*
+
+data class LoginData(val email: String, val password: String)
+
+class LoginEvent(override val data: LoginData) : DataEvent<LoginData>
+
+fun checkUserPassword(data: LoginData) = data.password == "qwerty"
+
+fun main() {
+    val machine = createStateMachine("Data states") {
+        logger = StateMachine.Logger { println(it) }
+
+        val accountFormState = dataState<LoginData>("accountForm") {
+            onEntry { println("login with :$data") }
+        }
+
+        initialState("loginForm") {
+            dataTransition<LoginEvent, LoginData> {
+                guard = { checkUserPassword(it.data) }
+                targetState = accountFormState
+            }
+        }
+    }
+
+    machine.processEvent(LoginEvent(LoginData("test@gmail.com", "qwerty")))
+}
