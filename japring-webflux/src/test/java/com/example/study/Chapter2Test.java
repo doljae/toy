61cf6c4cc83c46b56886b0b8ab54f11d1dafcee1
doljae <seok9211@naver.com>
2023-01-08 14:14:32 +0900

    Chapter 2, p.49

---
+++ japring-webflux/src/test/java/com/example/study/Chapter2Test.java
@@ -0,0 +1,25 @@
+package com.example.study;
+
+import org.junit.jupiter.api.Test;
+
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.disposables.Disposable;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+class Chapter2Test {
+
+    @Test
+    void test1() {
+        Disposable subscribe = Observable.create(emitter -> {
+            emitter.onNext("Hello");
+            emitter.onNext("World");
+            emitter.onNext("World");
+            emitter.onNext("World");
+            emitter.onNext("World");
+            emitter.onNext("World");
+            emitter.onNext("World");
+            emitter.onComplete();
+        }).subscribe(System.out::println, System.out::println, () -> System.out.println("Done"));
+    }
+}
